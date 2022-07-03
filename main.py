0667ab799282a4959ffe1cb69d972b4642024549
Andy Wu <awpats12@gmail.com>
2022-07-02 22:51:06 -0400

    Added GET and POST methods for users route

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 import json
-from flask import Flask
+from flask import Flask, jsonify, request
+
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return json.dumps({'first name': 'Andy',
-                       'last name': 'Wu'})
+# Sample data
+users = [
+    {'firstName': 'Andy', 'lastName': 'Wu'},
+    {'firstName': 'April', 'lastName': 'May'},
+    {'firstName': 'Crystal', 'lastName': 'Waters'}
+]
 
+@app.route('/users', methods=['GET'])
+def getPeople():
+    return jsonify(users)
+
+@app.route('/users', methods=['POST'])
+def setPerson():
+    users.append(request.get_json())
+    return '', 204
 
 if __name__ == "__main__":
-    app.run()
+    # debug=True allows for live server changes everytime the server code is updated
+    app.run(debug=True)
