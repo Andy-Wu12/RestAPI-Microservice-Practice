e67f4cbd1215249f4c691abcba0845a48c4f5958
Andy Wu <33229522+Andy-Wu12@users.noreply.github.com>
2022-07-06 02:00:27 -0400

    Merging with develop after completion of getPerson, updatePerson endpoints (#1)
    * Completed basic filtering for users route, updated insert to user non-deprecated insert_one function
    
    * updatePerson request on /users PUT route, global uid added for easy unique identification of users
    
    * API now allows for getting a single user by providing their uid in the users endpoint

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import json
 import pymongo
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
+from bson import ObjectId
 
 app = Flask(__name__)
 
@@ -8,11 +9,22 @@ app = Flask(__name__)
 client = pymongo.MongoClient('localhost', 27017)
 db = client.flask_rest_project
 
+# Use to generate unique user id
+# This should be in application server, but will be here until I create it
+num_users = db.users.count_documents({})
+
 @app.route('/users', methods=['GET'])
 def getPeople():
     user_collection = db.users
-    docs_cursor = user_collection.find()
     data = []
+
+    # Get all query parameters.
+    # Currently only handles filtering by fields separated by &
+    query_dict = {}
+    for param, value in request.args.items():
+        query_dict[param] = value
+
+    docs_cursor = user_collection.find(query_dict)
 
     # Convert oid to string to allow for jsonify to work properly
     for doc in docs_cursor:
@@ -21,12 +33,34 @@ def getPeople():
 
     return jsonify(data)
 
+@app.route('/users/<int:uid>', methods=['GET'])
+def getPerson(uid):
+    user_data = db.users.find_one({"uid": uid})
 
-# UIDs should be auto-generated, usually done by database with some sort of AUTO_INCREMENT
-# Bash script automatically sets every uid to -1 for now, until I get database set up
+    if user_data:
+        user_data['_id'] = str(user_data['_id'])
+        return jsonify(user_data)
+
+    return jsonify({'Error': 'No user with that id was found!'})
+
 @app.route('/users', methods=['POST'])
-def setPerson():
-    db.users.insert(request.get_json())
+def addPerson():
+    global num_users
+
+    json_data = request.get_json()
+    num_users += 1
+    json_data['uid'] = num_users
+
+    db.users.insert_one(json_data)
+    return '', 204
+
+@app.route('/users', methods=['PUT'])
+def updatePerson():
+    # uid uniquely identifies user, so query db with that to get correct document
+    updated_data = request.get_json()
+    user_id = updated_data['uid']
+
+    db.users.update_one({'uid': user_id}, {'$set': updated_data})
     return '', 204
 
 if __name__ == "__main__":
